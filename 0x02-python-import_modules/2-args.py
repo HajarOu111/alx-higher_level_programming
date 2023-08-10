0039d5e9b91801fa75ada76a8aa439569580cbf5
HAJAR OUAKRIM <assma941@gmail.com>
2023-08-10 16:29:18 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,8 @@ if argc < 2:
     print("{} arguments.".format(argc - 1))
 else:
     if argc == 2:
-        print("{} arguments:".format(argc - 1))
+        print("{} argument:".format(argc - 1))
     else:
         print("{} arguments:".format(argc - 1))
     for i in range(1, argc):
         print("{}: {}".format(i, argv[i]))
-
