a40880b6d37e10a98bf9022197e93391ed3900af
HAJAR OUAKRIM <assma941@gmail.com>
2023-08-18 16:40:27 -0700

    task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,11 +3,10 @@ def weight_average(my_list=[]):
     average = 0
     TotalWeight = 0
     TotalScore = 0
-    if len(my_list) <= 0:
+    if (len(my_list) <= 0):
         return 0
     for item in my_list:
         score, weight = item
         TotalWeight = TotalWeight + weight
         TotalScore = TotalScore + score * weight
     return TotalScore / TotalWeight
-    
