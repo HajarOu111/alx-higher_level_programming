c3f021c8fb12f6fffc4488f51fb18a0178973a68
HAJAR OUAKRIM <assma941@gmail.com>
2023-08-28 18:21:20 -0700

    task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as ex:
-        sys.stderr.write("Exception:")
+        sys.stderr.write("Exception: ")
         sys.stderr.write(ex.args[0])
         sys.stderr.write("\n")
         return False
