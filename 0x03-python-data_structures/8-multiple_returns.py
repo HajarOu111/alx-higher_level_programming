2c0c5ec326f9640ed9f1527b30e9376d5489969f
HAJAR OUAKRIM <assma941@gmail.com>
2023-08-29 17:18:29 -0700

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    new_tuple = ()
-    if len(sentence) > 0:
-        new_tuple = (len(sentence), sentence[0])
-    else:
-        None
+    new_tuple = (len(sentence), sentence[0] if len(sentence) > 0 else None)
     return new_tuple
