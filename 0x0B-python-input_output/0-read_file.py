53f85eb9ead3e462a5d926b3918e9551b5b85ea9
HAJAR OUAKRIM <assma941@gmail.com>
2023-09-12 16:21:06 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" This function reads a text file (UTF8) 
+and prints it to stdout
+"""
+
+
 def read_file(filename=""):
+    """ Opens the file in read mode """
     with open('my_file_0.txt', 'r') as file:
         file_content = file.read()
     print(file_content)
