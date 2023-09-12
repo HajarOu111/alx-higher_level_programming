6ab54f71ce3678de1047fabb52bfac18c7612a6e
HAJAR OUAKRIM <assma941@gmail.com>
2023-09-12 16:27:40 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ and prints it to stdout
 
 def read_file(filename=""):
     """ Opens the file in read mode """
-    with open(filename, encoding='utf-8') as file:
-        for line in file:
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
             print(line, end='')
