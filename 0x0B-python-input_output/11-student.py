26fec60ef3e0024e6bafc66ff44debfc9e09b8e6
HAJAR OUAKRIM <assma941@gmail.com>
2023-09-12 19:54:13 -0700

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" This class defines a student. """
+
+
+class Student:
+    """ Initialization of class student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ A function that retrieves a dictionary representation of a Student,
+        Retrieves only attribute names contained in this list if attrs is a
+        list of strings.
+        """
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of Student.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
