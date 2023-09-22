56081e4e5e7bc817b2a2a5a27a88a554b72c70f3
HAJAR OUAKRIM <assma941@gmail.com>
2023-09-22 15:38:49 -0700

    task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 from models.base import Base
-''' This class  inherits from Base.
-'''
+''' This class  inherits from Base '''
 
 class Rectangle(Base):
     ''' Child class '''
+
     def __init__(self, width, height, x=0, y=0, id=None):
         ''' Class constructor '''
         super().__init__(id)
