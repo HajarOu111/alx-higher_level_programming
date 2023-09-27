6891f1b868ee5ad257594d47a87d96a05806beca
HAJAR OUAKRIM <assma941@gmail.com>
2023-09-27 13:37:05 -0700

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A class BaseGeometry. """
+
+
+class BaseGeometry:
+    """ Public instance method """
+
+    def area(self):
+        """ if area is not implemented """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """ validates value """
+        if type(value) != int:
+            raise TypeError('{} must be an integer'.format(value))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
