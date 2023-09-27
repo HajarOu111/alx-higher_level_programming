9c1036ae784db7e71063612ba3fe2eab14489827
HAJAR OUAKRIM <assma941@gmail.com>
2023-09-27 16:39:54 -0700

    task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" A subclass that inherites from BaseGeometry. """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Constructor of the class """
+
+    def __init__(self, width, height):
+        """ The constructor of the superclass
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ if area is implemented """
+        return self.__width * self.__height
+        
+    def __str__(self):
+        """ returns the string """
+        string = "[" + (str(self.__class__.__name__)) + "] " 
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
