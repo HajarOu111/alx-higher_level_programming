bff7694ee37f33dde87100c3a848433d08eef7cd
HAJAR OUAKRIM <assma941@gmail.com>
2023-09-27 12:35:32 -0700

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This class inherits from list """
+
+
+
+
+
+class MyList(list):
+    """ Constructor of this subclass """
+    def ___init__(self):
+        super().__init__(self)
+
+    def print_sorted(self):
+        """ Public instance method that  prints the list, but sorted """
+        print(sorted(self))
